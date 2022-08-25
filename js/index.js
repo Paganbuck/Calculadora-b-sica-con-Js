6ecc9302310c535a6411ff8f6518e7f4e411a7b3
Juan Diego Bedoya <jdbedoya928@gmail.com>
2022-08-24 20:24:08 -0500

    Subiendo el proyecto

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function limpiar(){
+    document.getElementById('miFormulario').reset();
+}
+
+function sumar(){
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el_resultado').innerHTML = x+y;
+}
+
+function restar(){
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el_resultado').innerHTML = x-y;
+}
+
+function multiplicar(){
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el_resultado').innerHTML = x*y;
+}
+
+function dividir(){
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el_resultado').innerHTML = x/y;
+}
